293c0a80c0424e0411666ed1c12121fc17621495
TDRamos <tdvramos@gmail.com>
2017-08-17 19:20:16 +0100

    teste commit

--- Alu_logico.vhd
+++ Alu_logico.vhd
@@ -1,20 +1,16 @@
 ----------------------------------------------------------------------------------
 -- Project Name: PDS16fpga
--- 	Autors:	  João Botelho nº31169
---					  Tiago Ramos  nº00000
--- Create Date:  18:59:41 04/27/2017 
+
+-- Autors:	  João Botelho nº31169
+--				  Tiago Ramos  nº32125
+
 -- Module Name:  Alu_Logico - Descrição Hardware
--- Target Devices: 
--- Version: V.0.1
+
 -- Description: 
 -- Este modulo é referente ao bloco existente na ALU do PDS16 com o nome "Lógico".
 -- Tem como objetivo diponibilizar as seguintes funções lógicas.
 -- Operações Lógicas AND, OR, XOR e NOT
 --
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
 -- Additional Comments: 
 --
 ----------------------------------------------------------------------------------
@@ -25,14 +21,10 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use work.pds16_types.ALL;
 
 entity Alu_Logico is
-    Port ( Input_B : in  bit_16;
-           Input_A : in  bit_16;
-           Op : in  STD_LOGIC_VECTOR(1 downto 0);
-           Output : out  bit_16
---			  OpA : in  STD_LOGIC_VECTOR(15 downto 0);
---         OpB_Alu : in  STD_LOGIC_VECTOR(15 downto 0);
---         Func : in  STD_LOGIC_VECTOR(1 downto 0); -- Func[3] = IR11, Func[4] = 12
---         Result_logico : out  STD_LOGIC_VECTOR(15 downto 0)
+    Port ( Input_A : in  STD_LOGIC_VECTOR(15 downto 0);
+           Input_B : in  STD_LOGIC_VECTOR(15 downto 0);
+           Op : in  STD_LOGIC_VECTOR(1 downto 0); -- IR12 IR11
+           Output : out  STD_LOGIC_VECTOR(15 downto 0)
 			 );
 end Alu_Logico;
 
@@ -46,7 +38,7 @@ begin
 					when "01" => Output <= (Input_A OR Input_B);  --ORL
 					when "10" => Output <= (Input_A XOR Input_B); --XRL
 					when "11" => Output <= (NOT Input_A);			 --NOT
-					when others => Output <= (Input_A AND Input_B);
+					when others => Output <= "0000000000000000";
 				end case;
 	end process;
 
